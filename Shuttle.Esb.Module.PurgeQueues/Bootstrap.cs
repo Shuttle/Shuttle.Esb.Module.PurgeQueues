29dbe5d2e024ec60c9cc281166596d2acb99f5dc
Eben <me@ebenroux.co.za>
2017-03-19 17:00:54 +0200

    - bootstrapping

---
+++ Shuttle.Esb.Module.PurgeQueues/Bootstrap.cs
@@ -0,0 +1,41 @@
+﻿using Shuttle.Core.Infrastructure;
+
+namespace Shuttle.Esb.Module.PurgeQueues
+{
+	public class Bootstrap :
+		IComponentRegistryBootstrap,
+		IComponentResolverBootstrap
+	{
+		private static bool _registryBootstrapCalled;
+		private static bool _resolverBootstrapCalled;
+
+		public void Register(IComponentRegistry registry)
+		{
+			Guard.AgainstNull(registry, "registry");
+
+			if (_registryBootstrapCalled)
+			{
+				return;
+			}
+
+			registry.AttemptRegister<PurgeQueuesModule>();
+			registry.AttemptRegister<PurgeQueuesObserver>();
+
+			_registryBootstrapCalled = true;
+		}
+
+		public void Resolve(IComponentResolver resolver)
+		{
+			Guard.AgainstNull(resolver, "resolver");
+
+			if (_resolverBootstrapCalled)
+			{
+				return;
+			}
+
+			resolver.Resolve<PurgeQueuesModule>();
+
+			_resolverBootstrapCalled = true;
+		}
+	}
+}
